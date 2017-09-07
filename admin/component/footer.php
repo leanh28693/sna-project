81d691fc24ef286b2d6e686f3f1cc3024adc2305
GitHub <leanh280693@gmail.com>
2017-09-07 11:07:09 +0700

    first

---
+++ admin/component/footer.php
@@ -0,0 +1,8 @@
+    <script src="<?=URL_PUBLIC;?>js/jquery.filtertable.js"></script>
+    <script>
+    $(document).ready(function() {
+        $('table').filterTable();
+    });
+    </script>
+</body>
+</html>
